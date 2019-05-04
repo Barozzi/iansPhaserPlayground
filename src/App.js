7a2651a7328080586c0d7d0b19789834358c2dbe
Gregory Barozzi <gregbarozzi@gmail.com>
2019-05-04 10:01:35 -0700

    refactor to make more dry

--- src/App.js
+++ src/App.js
@@ -11,29 +11,12 @@ class App extends Component {
     this.state = {
       currentGame: GAMES.demo,
     }
-    this.setGamePandaDemo = this.setGamePandaDemo.bind(this);
-    this.setGameSurfDemo = this.setGameSurfDemo.bind(this);
-    this.setGameDefender = this.setGameDefender.bind(this);
-    this.setGameMassAttack = this.setGameMassAttack.bind(this);
-    this.setGameFlood = this.setGameFlood.bind(this);
+    this.setGame = this.setGame.bind(this);
   }
 
-  setGamePandaDemo() {
-    this.setState(Object.assign({}, this.state, {currentGame: GAMES.demo}))
+  setGame(game) {
+    this.setState(Object.assign({}, this.state, {currentGame: game}))
   }
-  setGameSurfDemo() {
-    this.setState(Object.assign({}, this.state, {currentGame: GAMES.surfDemo}))
-  }
-  setGameDefender() {
-    this.setState(Object.assign({}, this.state, {currentGame: GAMES.defender}))
-  }
-  setGameMassAttack() {
-    this.setState(Object.assign({}, this.state, {currentGame: GAMES.massAttack}))
-  }
-  setGameFlood() {
-    this.setState(Object.assign({}, this.state, {currentGame: GAMES.flood}))
-  }
-
 
   render() {
     return (
@@ -41,14 +24,12 @@ class App extends Component {
         <header className="App-header">
           <h2>Ian's Awesome Games</h2>
           <div className="button-bar">
-            <Button className="btn" onClick={this.setGamePandaDemo} text={GAMES.demo} />
-            <Button className="btn" onClick={this.setGameSurfDemo} text={GAMES.surfDemo} />
-            <Button className="btn" onClick={this.setGameDefender} text={GAMES.defender} />
-            <Button className="btn" onClick={this.setGameMassAttack} text={GAMES.massAttack} />
-            <Button className="btn" onClick={this.setGameFlood} text={GAMES.flood} />
+            {Object.values(GAMES).map(game => (
+              <Button id={ game.name } className='btn' onClick={ () => this.setGame(game) } text={ game.name } />
+            ))}
           </div>
         </header>
-        <GameLoader currentGame={this.state.currentGame} />
+          <GameLoader currentGame={this.state.currentGame} />
       </div>
     );
   }
